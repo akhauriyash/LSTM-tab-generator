0419d624dfb03b0f9ade462b41125912792a8e9c
Yash Akhauri <akhauri.yash@gmail.com>
2020-05-29 15:24:50 +0530

    Black reformatter used

--- arraysaver.py
+++ arraysaver.py
@@ -3,7 +3,7 @@ import time
 
 #   Convert input array to one-hot encoding
 def to_categorical(y, num_classes=None):
-    y = np.array(y, dtype='int')
+    y = np.array(y, dtype="int")
     input_shape = y.shape
     if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
         input_shape = tuple(input_shape[:-1])
@@ -16,16 +16,31 @@ def to_categorical(y, num_classes=None):
     output_shape = input_shape + (num_classes,)
     categorical = np.reshape(categorical, output_shape)
     return categorical
-    
+
+
 #### REPLACING 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24
 #### from the dataset, so that one-hot classification can be done.
-####           # , $ , > , ^ , & , * , ( , ) , _ , + , = , } , { , " , : 
+####           # , $ , > , ^ , & , * , ( , ) , _ , + , = , } , { , " , :
 a = time.time()
-replacements = {'10': '#-', '11': '$-', '12': '>-', '13': '^', '14': '&-',
-                '15': '*-', '16': '(-', '17': ')-', '18': '_-', '19': '+-',
-                '20': '=-', '21': '}-', '22': '{-', '23': '"-', '24': ':-'}
+replacements = {
+    "10": "#-",
+    "11": "$-",
+    "12": ">-",
+    "13": "^",
+    "14": "&-",
+    "15": "*-",
+    "16": "(-",
+    "17": ")-",
+    "18": "_-",
+    "19": "+-",
+    "20": "=-",
+    "21": "}-",
+    "22": "{-",
+    "23": '"-',
+    "24": ":-",
+}
 print("########## Starting replacements ##########")
-with open('input.txt', 'r') as infile, open('stage1.txt', 'w') as outfile:
+with open("input.txt", "r") as infile, open("stage1.txt", "w") as outfile:
     for segment in infile:
         for src, target in replacements.items():
             segment = segment.replace(src, target)
@@ -33,11 +48,11 @@ with open('input.txt', 'r') as infile, open('stage1.txt', 'w') as outfile:
         outfile.write(segment)
 print("Replaced numbers greater than 10 with symbols")
 print("########## Starting dataset alignment ##########")
-with open('stage1.txt', 'r') as file, open('stage2.txt', 'w') as writeto:
+with open("stage1.txt", "r") as file, open("stage2.txt", "w") as writeto:
     data = file.read()
     for line in data.split("%"):
         c = 0
-        if(len(line) > 7):
+        if len(line) > 7:
             crop = 0
             lenc = []
             for item in line.split("\n"):
@@ -47,12 +62,12 @@ with open('stage1.txt', 'r') as file, open('stage2.txt', 'w') as writeto:
             for item in line.split("\n"):
                 c += 1
                 writeto.write(item[:min_len])
-                if(c!=7):
+                if c != 7:
                     writeto.write("\n")
             writeto.write("%")
 print("Written aligned dataset")
 print("########## Starting dataset reduction ##########")
-with open('stage2.txt', 'r') as file, open('dataset.txt', 'w') as writeto:
+with open("stage2.txt", "r") as file, open("dataset.txt", "w") as writeto:
     for segment in data.split("\n%\n"):
         if len(segment) > 30:
             ns = []
@@ -61,12 +76,12 @@ with open('stage2.txt', 'r') as file, open('dataset.txt', 'w') as writeto:
             numchk = np.zeros(min(ns))
             for line in segment.split("\n"):
                 if len(line[1:]) > 7:
-                    for index, character in enumerate(line[1:min(ns)]):
+                    for index, character in enumerate(line[1 : min(ns)]):
                         if character == "-":
                             numchk[index] += 1
             for line in segment.split("\n"):
                 if len(line[1:]) > 7:
-                    for index, character in enumerate(line[1:min(ns)]):
+                    for index, character in enumerate(line[1 : min(ns)]):
                         if numchk[index] != 6:
                             writeto.write(character)
                     writeto.write("\n")
@@ -75,58 +90,84 @@ with open('stage2.txt', 'r') as file, open('dataset.txt', 'w') as writeto:
             pass
 print("Final processing time:  " + str(time.time() - a))
 a = time.time()
-file = open('dataset.txt', 'r')
+file = open("dataset.txt", "r")
 data = file.read()
 print("Read time:  " + str(time.time() - a))
 
 # Allowed characters contain everything, including the None state "-"
-allowed = ['"', '#', '$', '&', '(', ')', '*', '+', '-', '0', '1', '2', '3',
-           '4', '5', '6', '7', '8', '9', ':', '=', '>', '^', '_', '{', '}']
+allowed = [
+    '"',
+    "#",
+    "$",
+    "&",
+    "(",
+    ")",
+    "*",
+    "+",
+    "-",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    ":",
+    "=",
+    ">",
+    "^",
+    "_",
+    "{",
+    "}",
+]
 
-train = [[],[],[],[],[],[]]
-true = [[],[],[],[],[],[]]
+train = [[], [], [], [], [], []]
+true = [[], [], [], [], [], []]
 charsset = sorted(list(set(data)))
 chars = []
 for s in charsset:
     if s in allowed:
         chars.append(s)
-        
-# Character to integer mapping 
-charint = dict((char, ints) for ints, char in enumerate(chars))
-intchar = dict((ints,char) for ints, char in enumerate(chars))
 
-# Set your sequence length of the generated dataset here. 
+# Character to integer mapping
+charint = dict((char, ints) for ints, char in enumerate(chars))
+intchar = dict((ints, char) for ints, char in enumerate(chars))
+
+# Set your sequence length of the generated dataset here.
 # This must be the same in your network file
 seqlen = 16
 
 # We go through every segment in the dataset,
 # traverse every line and add it to one big list for each string.
 a = time.time()
-e = 'e'
-B = 'B'
-G = 'G'
-D = 'D'
-A = 'A'
-E = 'E'
+e = "e"
+B = "B"
+G = "G"
+D = "D"
+A = "A"
+E = "E"
 for segment in data.split("\n%\n"):
     note = 0
     for line in segment.split("\n"):
-        if(note==0):
+        if note == 0:
             e += line
             note += 1
-        elif(note==1):
+        elif note == 1:
             B += line
             note += 1
-        elif(note==2):
+        elif note == 2:
             G += line
             note += 1
-        elif(note==3):
+        elif note == 3:
             D += line
             note += 1
-        elif(note==4):
+        elif note == 4:
             A += line
             note += 1
-        elif(note==5):
+        elif note == 5:
             E += line
             note += 1
 e = e.replace("e", "")
@@ -141,18 +182,18 @@ size = min(len(e), len(B), len(G), len(D), len(A), len(E))
 
 a = time.time()
 for i in range(0, size - seqlen - 4, 4):
-    train[0].append([charint[char] for char in e[i:i+seqlen]])
-    train[1].append([charint[char] for char in B[i:i+seqlen]])
-    train[2].append([charint[char] for char in G[i:i+seqlen]])
-    train[3].append([charint[char] for char in D[i:i+seqlen]])
-    train[4].append([charint[char] for char in A[i:i+seqlen]])
-    train[5].append([charint[char] for char in E[i:i+seqlen]])
-    true[0].append([charint[char] for char in e[i+seqlen]])
-    true[1].append([charint[char] for char in B[i+seqlen]])
-    true[2].append([charint[char] for char in G[i+seqlen]])
-    true[3].append([charint[char] for char in D[i+seqlen]])
-    true[4].append([charint[char] for char in A[i+seqlen]])
-    true[5].append([charint[char] for char in E[i+seqlen]])
+    train[0].append([charint[char] for char in e[i : i + seqlen]])
+    train[1].append([charint[char] for char in B[i : i + seqlen]])
+    train[2].append([charint[char] for char in G[i : i + seqlen]])
+    train[3].append([charint[char] for char in D[i : i + seqlen]])
+    train[4].append([charint[char] for char in A[i : i + seqlen]])
+    train[5].append([charint[char] for char in E[i : i + seqlen]])
+    true[0].append([charint[char] for char in e[i + seqlen]])
+    true[1].append([charint[char] for char in B[i + seqlen]])
+    true[2].append([charint[char] for char in G[i + seqlen]])
+    true[3].append([charint[char] for char in D[i + seqlen]])
+    true[4].append([charint[char] for char in A[i + seqlen]])
+    true[5].append([charint[char] for char in E[i + seqlen]])
 
 learn = np.array([np.array(xi) for xi in train])
 test = np.asarray([np.array(xi) for xi in true])
@@ -164,30 +205,30 @@ print("Test shape: " + str(test.shape))
 del train, true, e, B, G, D, A, E
 
 a = time.time()
-e = np.expand_dims(np.array(np.asarray(learn[0])[:size, :]), axis = 0)
-B = np.expand_dims(np.array(np.asarray(learn[1])[:size, :]), axis = 0)
-G = np.expand_dims(np.array(np.asarray(learn[2])[:size, :]), axis = 0)
-D = np.expand_dims(np.array(np.asarray(learn[3])[:size, :]), axis = 0)
-A = np.expand_dims(np.array(np.asarray(learn[4])[:size, :]), axis = 0)
-E = np.expand_dims(np.array(np.asarray(learn[5])[:size, :]), axis = 0)
-X = np.concatenate((e, B, G, D, A, E), axis = 0)/float(len(chars))
+e = np.expand_dims(np.array(np.asarray(learn[0])[:size, :]), axis=0)
+B = np.expand_dims(np.array(np.asarray(learn[1])[:size, :]), axis=0)
+G = np.expand_dims(np.array(np.asarray(learn[2])[:size, :]), axis=0)
+D = np.expand_dims(np.array(np.asarray(learn[3])[:size, :]), axis=0)
+A = np.expand_dims(np.array(np.asarray(learn[4])[:size, :]), axis=0)
+E = np.expand_dims(np.array(np.asarray(learn[5])[:size, :]), axis=0)
+X = np.concatenate((e, B, G, D, A, E), axis=0) / float(len(chars))
 
-y_e = np.expand_dims(test[0][:size], axis = 0)
-y_B = np.expand_dims(test[1][:size], axis = 0)
-y_G = np.expand_dims(test[2][:size], axis = 0)
-y_D = np.expand_dims(test[3][:size], axis = 0)
-y_A = np.expand_dims(test[4][:size], axis = 0)
-y_E = np.expand_dims(test[5][:size], axis = 0)
-y_array = np.concatenate((y_e, y_B, y_G, y_D, y_A, y_E), axis = 0)
-y2 = to_categorical(y_array)            # (6, 3321076, 26)
-inp = np.swapaxes(X, 0, 1)                      
+y_e = np.expand_dims(test[0][:size], axis=0)
+y_B = np.expand_dims(test[1][:size], axis=0)
+y_G = np.expand_dims(test[2][:size], axis=0)
+y_D = np.expand_dims(test[3][:size], axis=0)
+y_A = np.expand_dims(test[4][:size], axis=0)
+y_E = np.expand_dims(test[5][:size], axis=0)
+y_array = np.concatenate((y_e, y_B, y_G, y_D, y_A, y_E), axis=0)
+y2 = to_categorical(y_array)  # (6, 3321076, 26)
+inp = np.swapaxes(X, 0, 1)
 y2 = np.swapaxes(y2, 0, 1)
-y = np.empty((y2.shape[0], y2.shape[1]*y2.shape[2]))
+y = np.empty((y2.shape[0], y2.shape[1] * y2.shape[2]))
 for i in range(y2.shape[0]):
     y[i] = y2[i, :, :].flatten()
 y = np.squeeze(y)
-print("Shape of input: ", inp.shape)    # Shape of input:  (3321076, 6, 16)
-print("Shape of output: ", y.shape)     # Shape of output: (3321076, 156)
+print("Shape of input: ", inp.shape)  # Shape of input:  (3321076, 6, 16)
+print("Shape of output: ", y.shape)  # Shape of output: (3321076, 156)
 
 np.save("smallinpusssst", inp)
 np.save("smalltrusssse", y)
--- network.py
+++ network.py
@@ -10,22 +10,112 @@ import tensorflow as tf
 import numpy as np
 import tflearn, random, time
 
-chars = ['"', '#', '$', '&', '(', ')', '*', '+', '-', '0', '1', '2', '3', '4', 
-         '5', '6', '7', '8', '9', ':', '=', '>', '^', '_', '{', '}']
+chars = [
+    '"',
+    "#",
+    "$",
+    "&",
+    "(",
+    ")",
+    "*",
+    "+",
+    "-",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    ":",
+    "=",
+    ">",
+    "^",
+    "_",
+    "{",
+    "}",
+]
 
-replacements = {'10': '#', '11': '$', '12': '>', '13': '^', '14': '&',
-                '15': '*', '16': '(', '17': ')', '18': '_', '19': '+',
-                '20': '=', '21': '}', '22': '{', '23': '"', '24': ':'}
+replacements = {
+    "10": "#",
+    "11": "$",
+    "12": ">",
+    "13": "^",
+    "14": "&",
+    "15": "*",
+    "16": "(",
+    "17": ")",
+    "18": "_",
+    "19": "+",
+    "20": "=",
+    "21": "}",
+    "22": "{",
+    "23": '"',
+    "24": ":",
+}
 
 charint = dict((char, ints) for ints, char in enumerate(chars))
-intchar = dict((ints,char) for ints, char in enumerate(chars))
+intchar = dict((ints, char) for ints, char in enumerate(chars))
 
-charint = {'>': 21, '0': 9, '6': 15, '{': 24, '(': 4, '1': 10, '7': 16, '5': 14,
-        '#': 1, '"': 0, ')': 5, '*': 6, '+': 7, '}': 25, '4': 13, '-': 8, 
-        ':': 19, '2': 11, '3': 12, '^': 22, '9': 18, '8': 17, '&': 3, '$': 2, '_': 23, '=': 20}
-intchar = {0: '23', 1: '10', 2: '11', 3: '14', 4: '16', 5: '17', 6: '15', 7: '19', 8: '-', 
-           9: '0', 10: '1', 11: '2', 12: '3', 13: '4', 14: '5', 15: '6', 16: '7', 
-           17: '8', 18: '9', 19: '24', 20: '20', 21: '12', 22: '13', 23: '18', 24: '22', 25: '21'}
+charint = {
+    ">": 21,
+    "0": 9,
+    "6": 15,
+    "{": 24,
+    "(": 4,
+    "1": 10,
+    "7": 16,
+    "5": 14,
+    "#": 1,
+    '"': 0,
+    ")": 5,
+    "*": 6,
+    "+": 7,
+    "}": 25,
+    "4": 13,
+    "-": 8,
+    ":": 19,
+    "2": 11,
+    "3": 12,
+    "^": 22,
+    "9": 18,
+    "8": 17,
+    "&": 3,
+    "$": 2,
+    "_": 23,
+    "=": 20,
+}
+intchar = {
+    0: "23",
+    1: "10",
+    2: "11",
+    3: "14",
+    4: "16",
+    5: "17",
+    6: "15",
+    7: "19",
+    8: "-",
+    9: "0",
+    10: "1",
+    11: "2",
+    12: "3",
+    13: "4",
+    14: "5",
+    15: "6",
+    16: "7",
+    17: "8",
+    18: "9",
+    19: "24",
+    20: "20",
+    21: "12",
+    22: "13",
+    23: "18",
+    24: "22",
+    25: "21",
+}
 
 testing = 1
 if testing:
@@ -44,77 +134,89 @@ print("Data shape: ", inp.shape)
 print("True shape: ", y.shape)
 
 num_strings = 6
-seqlen = 16 
+seqlen = 16
 lstmhid = 156
 filename = "trained"
 
 #   ---------------------------------------------------------------------------
 tf.reset_default_graph()
 net = tflearn.input_data(shape=[None, inp.shape[1], inp.shape[2]])
-net = tflearn.lstm(net, lstmhid, return_seq = True)
-net = tflearn.lstm(net, n_units = 2*lstmhid, dropout = 0.7, return_seq = True)
-net = tflearn.lstm(net, n_units = lstmhid)
+net = tflearn.lstm(net, lstmhid, return_seq=True)
+net = tflearn.lstm(net, n_units=2 * lstmhid, dropout=0.7, return_seq=True)
+net = tflearn.lstm(net, n_units=lstmhid)
 net = tflearn.reshape(net, (tf.shape(net)[0], len(chars), num_strings))
-net = tflearn.conv_1d(net, filter_size = 1, nb_filter = 256, activation='relu')
-net = tflearn.conv_1d(net, filter_size = 1, nb_filter = 6)
+net = tflearn.conv_1d(net, filter_size=1, nb_filter=256, activation="relu")
+net = tflearn.conv_1d(net, filter_size=1, nb_filter=6)
 print(net.shape)
-net = tf.nn.softmax(net, dim = 1)
+net = tf.nn.softmax(net, dim=1)
 
-net = tflearn.regression(net, optimizer='adam',
-                         learning_rate=0.001, loss ='categorical_crossentropy')
+net = tflearn.regression(
+    net, optimizer="adam", learning_rate=0.001, loss="categorical_crossentropy"
+)
 
-model = tflearn.DNN(net, checkpoint_path = 'guitarmodel/model.tfl.ckpt',
-                    tensorboard_verbose=0, tensorboard_dir='log/')
+model = tflearn.DNN(
+    net,
+    checkpoint_path="guitarmodel/model.tfl.ckpt",
+    tensorboard_verbose=0,
+    tensorboard_dir="log/",
+)
 
 print("Model fitting")
 ckpt = "guitarmodel/model.tfl.ckpt-170000"
 print(tf.test.gpu_device_name())
 #   Training
-if (1):
+if 1:
     restore = 0
-    if(restore):
-        model.load(ckpt) 
-    model.fit(inp, y, snapshot_step=5000, show_metric = True, snapshot_epoch=False,
-               n_epoch=5, batch_size = 64)
+    if restore:
+        model.load(ckpt)
+    model.fit(
+        inp,
+        y,
+        snapshot_step=5000,
+        show_metric=True,
+        snapshot_epoch=False,
+        n_epoch=5,
+        batch_size=64,
+    )
     model.save(filename)
-    
+
 #   ---------------------------------------------------------------------------
 #   Memory feed
-elif(0):
+elif 0:
     print("Memory seed")
     model.load(ckpt)
     for _ in range(5):
-        seq = 2*seqlen
+        seq = 2 * seqlen
         if testing == 0:
             index = random.randint(1, 100000)
         else:
             index = random.randint(1, 100000)
-        print("seed:  " + str(index), end = '  shape:   ')
+        print("seed:  " + str(index), end="  shape:   ")
         test = inp[index, :, :]
-        test = np.expand_dims(test, axis = 0)    
+        test = np.expand_dims(test, axis=0)
         print("e\tB\tG\tD\tA\tE")
         for p in range(seqlen):
-            arr = len(chars)*test[0,:,p]
+            arr = len(chars) * test[0, :, p]
             for item in arr:
-                print(intchar[int(item)], end = '\t')
+                print(intchar[int(item)], end="\t")
             print("")
-        print('___________________________________________')
+        print("___________________________________________")
         for i in range(seq):
-            append = np.empty((1,6,1))
+            append = np.empty((1, 6, 1))
             k = model.predict(test)
             for temp in range(6):
-                append[0,temp,0] = np.argmax(k[0, :, temp])/float(len(chars))
+                append[0, temp, 0] = np.argmax(k[0, :, temp]) / float(len(chars))
             test = test[0, :, 1:]
-            test = np.expand_dims(test, axis = 0)
-            test = np.append(test, append, axis = 2)
-            for item in append[0,:,0]:
-                print((intchar[int(item*len(chars))]), end="\t")
+            test = np.expand_dims(test, axis=0)
+            test = np.append(test, append, axis=2)
+            for item in append[0, :, 0]:
+                print((intchar[int(item * len(chars))]), end="\t")
             print("")
         print("x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x")
-        
+
 #   Random feed
-elif(0):
-    print("Random seeds") 
+elif 0:
+    print("Random seeds")
     model.load(ckpt)
     seq = seqlen
     for _ in range(5):
@@ -125,12 +227,12 @@ elif(0):
             else:
                 index = random.randint(1, 1000)
             test = inp[index, :, :]
-            test = np.expand_dims(test, axis = 0)            
-            append = np.empty((1,6,1))
+            test = np.expand_dims(test, axis=0)
+            append = np.empty((1, 6, 1))
             k = model.predict(test)
             for temp in range(6):
-                append[0,temp,0] = np.argmax(k[0,:,temp])/float(len(chars))
-            for item in append[0,:,0]:
-                print((intchar[int(item*len(chars))]), end="\t")
+                append[0, temp, 0] = np.argmax(k[0, :, temp]) / float(len(chars))
+            for item in append[0, :, 0]:
+                print((intchar[int(item * len(chars))]), end="\t")
             print("")
         print("x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x")
